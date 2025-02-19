c742902ee57ad4b90e268cc59bd216976e523acf
Sona Varghese <svarghese@thewellnessway.com>
2025-02-19 14:18:54 +0530

    Latest_Terraform

---
+++ main.tf
@@ -0,0 +1,31 @@
+terraform {
+    backend "s3" {
+    bucket         = "my-terraform-state-bucket-latest"
+    key    = "terraform/statefile.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "terraform-lock"
+
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  required_version = "~> 1.5"
+}
+
+provider "aws" {
+  default_tags {
+    tags = {
+      Environment = "training"
+    }
+  }
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "sona-tf-bucket"
+  tags = {
+    CreadtedBy = "Sona"
+  }
+}
