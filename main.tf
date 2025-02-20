c63eb7f0f8638cffd81bf26fb71034617a03468b
sona1997 <78351885+sona1997@users.noreply.github.com>
2025-02-20 12:35:52 +0530

    latest_branch

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
     backend "s3" {
     bucket         = "my-terraform-state-bucket-latest"
-    key    = "terraform/jenkins/statefile.tfstate"
+    key    = "terraform/jenkins/prod/statefile.tfstate"
     region = "us-east-1"
     dynamodb_table = "terraform-lock"
 
@@ -18,13 +18,13 @@ terraform {
 provider "aws" {
   default_tags {
     tags = {
-      Environment = "training"
+      Environment = "prod"
     }
   }
 }
 
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = "sona-tf-bucket-training"
+  bucket = "sona-tf-bucket-prod"
   tags = {
     CreadtedBy = "Sona"
   }
